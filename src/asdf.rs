c573c4e964e722f07a134c75260212d16c299819
kurotetsuka <kurotetsuka@gmail.com>
2014-10-12 15:09:15 -0400

    resetting asdf.rs

--- src/asdf.rs
+++ src/asdf.rs
@@ -1,9 +1,2 @@
-fn main(){
-	let original : [u8, ..7] = [ 0u8, 1, 2, 3, 4, 5, 6];
-	let original_copy = copy( original);}
 
-fn copy( slice : &[u8]) -> [u8] {
-	let result : Vec<u8> = Vec::new();
-	for i in range( 0, slice.len()) {
-		result.push( slice[i]);}
-	return result.as_slice();}
+fn main(){}
